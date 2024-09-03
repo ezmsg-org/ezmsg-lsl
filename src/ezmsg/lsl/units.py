1e75f0d2dcf75b5f2ee60d00c74c0a50526d9418
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-09-03 16:47:15 -0400

    Move AxisArray monkey patch to `util` module so downstream can use it without importing pylsl.

--- src/ezmsg/lsl/units.py
+++ src/ezmsg/lsl/units.py
@@ -5,22 +5,9 @@ import typing
 
 import numpy as np
 import pylsl
-
 import ezmsg.core as ez
-from ezmsg.util.messages.axisarray import AxisArray
 
-
-@dataclass
-class CustomAxis(AxisArray.Axis):
-    labels: typing.List[str] = field(default_factory=lambda: [])
-
-    @classmethod
-    def SpaceAxis(cls, labels: typing.List[str]):  # , locs: typing.Optional[npt.NDArray] = None):
-        return cls(unit="mm", labels=labels)
-
-
-# Monkey-patch AxisArray with our customized Axis
-AxisArray.Axis = CustomAxis
+from .util import AxisArray
 
 
 # Reproduce pylsl.string2fmt but add float64 for more familiar numpy usage
