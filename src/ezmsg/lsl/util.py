7c6183ec3bad41be4b92531836a955a3e76c3ce8
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-11-12 21:42:26 -0500

    Make util.ClockSync more robust and testable.

--- src/ezmsg/lsl/util.py
+++ src/ezmsg/lsl/util.py
@@ -1,8 +1,24 @@
 import asyncio
 import time
+import typing
 
 import numpy as np
+import numpy.typing as npt
 import pylsl
+
+
+async def collect_timestamp_pairs(npairs: int = 4) -> typing.Tuple[np.ndarray, np.ndarray]:
+    xs = []
+    ys = []
+    for _ in range(npairs):
+        if _ % 2:
+            y, x = time.time(), pylsl.local_clock()
+        else:
+            x, y = pylsl.local_clock(), time.time()
+        xs.append(x)
+        ys.append(y)
+        await asyncio.sleep(0.001)
+    return np.array(xs), np.array(ys)
 
 
 class ClockSync:
@@ -10,36 +26,60 @@ class ClockSync:
         self.alpha = alpha
         self.min_interval = min_interval
 
-        self.offset = 0.0
-        self.last_update = 0.0
+        self._offset: typing.Optional[float] = None
+        self._last_update = 0.0
         self.count = 0
 
+    @property
+    def offset(self) -> float:
+        return self._offset
+
+    @property
+    def last_updated(self) -> float:
+        return self._last_update
+
     async def update(self, force: bool = False, burst: int = 4) -> None:
-        dur_since_last = time.time() - self.last_update
+        """
+        Update the clock offset estimate. This should be called ~regularly in a long-running
+        asynchronous task.
+
+        Args:
+            force: Whether to force an update even if the minimum interval hasn't passed.
+            burst: The number of pairs to collect for this update.
+        """
+        dur_since_last = time.time() - self._last_update
         dur_until_next = self.min_interval - dur_since_last
         if force or dur_until_next <= 0:
-            offsets = []
-            for ix, _ in enumerate(range(burst)):
-                if (self.count + ix) % 2:
-                    y, x = time.time(), pylsl.local_clock()
-                else:
-                    x, y = pylsl.local_clock(), time.time()
-                # TODO: Use adaptive linear fit instead of simple subtraction.
-                offsets.append(y - x)
-                self.last_update = y
-                await asyncio.sleep(0.001)
-            offset = np.mean(offsets)
-
-            if self.count > 0:
-                # Exponential decay smoothing
-                offset = (1 - self.alpha) * self.offset + self.alpha * offset
-            self.offset = offset
+            xs, ys = collect_timestamp_pairs(burst)
             self.count += burst
+            if burst > 0:
+                self._last_update = ys[-1]
+            offset = np.mean(ys - xs)
+            if self.offset is not None:
+                # Do exponential smoothing update.
+                self._offset = (1 - self.alpha) * self._offset + self.alpha * offset
+            else:
+                # First iteration. No smoothing.
+                self._offset = offset
         else:
             await asyncio.sleep(dur_until_next)
 
+    @typing.overload
     def lsl2system(self, lsl_timestamp: float) -> float:
-        return lsl_timestamp + self.offset
+        ...
+    @typing.overload
+    def lsl2system(self, lsl_timestamp: npt.NDArray[float]) -> npt.NDArray[float]:
+        ...
+    def lsl2system(self, lsl_timestamp):
+        # offset = system - lsl --> system = lsl + offset
+        return lsl_timestamp + self._offset
 
+    @typing.overload
     def system2lsl(self, system_timestamp: float) -> float:
-        return system_timestamp - self.offset
+        ...
+    @typing.overload
+    def system2lsl(self, system_timestamp: npt.NDArray[float]) -> npt.NDArray[float]:
+        ...
+    def system2lsl(self, system_timestamp):
+        # offset = system - lsl --> lsl = system - offset
+        return system_timestamp - self._offset
