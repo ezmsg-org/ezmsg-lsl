97c0aa6d73160e841c2b9cd4cb4efcf7db4f4a9e
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-11-19 14:47:15 -0500

    Refactor ClockSync so threading is optional.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,16 +1,24 @@
 import time
 
 import numpy as np
+import pytest
 
 from ezmsg.lsl.util import ClockSync, collect_timestamp_pairs
 
 
-def test_clock_sync():
+@pytest.mark.parametrize("own_thread", [True, False])
+def test_clock_sync(own_thread: bool):
     tol = 10e-3  # 1 msec
 
-    clock_sync = ClockSync()
-    # Let it run a bit to get a stable estimate.
-    time.sleep(1.0)
+    clock_sync = ClockSync(run_thread=own_thread)
+    if own_thread:
+        # Let it run a bit to get a stable estimate.
+        time.sleep(1.0)
+        clock_sync.stop()
+    else:
+        for ix in range(10):
+            clock_sync.run_once()
+            time.sleep(0.1)
 
     offsets = []
     for _ in range(10):
