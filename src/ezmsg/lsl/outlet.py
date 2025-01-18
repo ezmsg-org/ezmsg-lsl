1028e9585d5c59ff202c16a9381b084a11cbf242
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-01-18 11:58:11 -0500

    Refactor Outlet to have a core non-Unit processing object.

--- src/ezmsg/lsl/outlet.py
+++ src/ezmsg/lsl/outlet.py
@@ -34,6 +34,112 @@ class LSLOutletSettings(ez.Settings):
 
 class LSLOutletState(ez.State):
     outlet: typing.Optional[pylsl.StreamOutlet] = None
+    clock_sync: ClockSync = ClockSync(run_thread=False)
+    hash: int = 0
+
+
+class OutletProcessor:
+
+    def __init__(self, *args, settings: typing.Optional[LSLOutletSettings] = None, **kwargs):
+        if settings is None:
+            if len(args) > 0 and isinstance(args[0], LSLOutletSettings):
+                settings = args[0]
+            elif len(args) > 0 or len(kwargs) > 0:
+                settings = LSLOutletSettings(*args, **kwargs)
+            else:
+                settings = LSLOutletSettings()
+        self.settings = settings
+        self._state: LSLOutletState = LSLOutletState()
+
+    def __del__(self):
+        self.shutdown()
+
+    @property
+    def state(self) -> LSLOutletState:
+        return self._state
+
+    def shutdown(self):
+        if self._state.outlet is not None:
+            del self._state.outlet
+            self._state.outlet = None
+
+    def check_metadata(self, message: AxisArray) -> bool:
+        b_reset = False
+        b_reset = b_reset or self.state.outlet is None
+        fs = pylsl.IRREGULAR_RATE
+        sample_shape = message.data.shape
+        if "time" in message.axes:
+            if hasattr(message.axes["time"], "gain"):
+                fs = 1 / message.axes["time"].gain
+            time_ix = message.get_axis_idx("time")
+            sample_shape = message.data.shape[:time_ix] + message.data.shape[time_ix + 1:]
+        this_hash = hash((message.key, message.data.dtype, fs, sample_shape))
+        b_reset = b_reset or this_hash != self._state.hash
+        if b_reset:
+            self._state.hash = this_hash
+        return b_reset
+
+    def reset(self, message: AxisArray) -> None:
+        self.shutdown()
+
+        fs = pylsl.IRREGULAR_RATE
+        if "time" in message.axes and hasattr(message.axes["time"], "gain"):
+            fs = 1 / message.axes["time"].gain
+        out_shape = [_[0] for _ in zip(message.shape, message.dims) if _[1] != "time"]
+        out_size = int(np.prod(out_shape))
+        info = pylsl.StreamInfo(
+            name=self.settings.stream_name,
+            type=self.settings.stream_type,
+            channel_count=out_size,
+            nominal_srate=fs,
+            channel_format=string2fmt[str(message.data.dtype)],
+            source_id="",  # TODO: Generate a hash from name, type, channel_count, fs, fmt, other metadata...
+        )
+        # Add channel labels to the info desc.
+        if "ch" in message.axes and isinstance(
+                message.axes["ch"], AxisArray.CoordinateAxis
+        ):
+            ch_labels = message.axes["ch"].data
+            # TODO: or get ch_labels from self.settings.map_file
+            # TODO: if msg is multi-dim then construct labels by combining dims.
+            #  For now, labels only work if only output dims are "time", "ch"
+            if len(ch_labels) == out_size:
+                chans = info.desc().append_child("channels")
+                for ch in ch_labels:
+                    chan = chans.append_child("channel")
+                    chan.append_child_value("label", ch)
+                    # TODO: if self.settings.map_file: Add channel locations
+        self._state.outlet = pylsl.StreamOutlet(info)
+
+    def _process(self, message: AxisArray):
+        dat = message.data
+        if message.dims[0] != "time":
+            dat = np.moveaxis(dat, message.dims.index("time"), 0)
+
+        if not dat.flags.c_contiguous:
+            dat = np.ascontiguousarray(dat)
+        if not dat.flags.writeable:
+            # If there is a shared-memory-hop in the processing graph before this node then it has made
+            #  the numpy array non-writeable. We need to copy it to a new buffer.
+            dat = np.ascontiguousarray(dat).copy()
+
+        if hasattr(message.axes["time"], "data"):
+            ts = message.axes["time"].data
+        else:
+            ts = message.axes["time"].value(dat.shape[0])
+        ts = self._state.clock_sync.system2lsl(ts)
+        self._state.outlet.push_chunk(dat.reshape(dat.shape[0], -1), timestamp=ts)
+
+    def __call__(self, message: AxisArray):
+        if self.check_metadata(message):
+            self.reset(message)
+        return self._process(message)
+
+    def __iter__(self):
+        self._state: LSLOutletState = LSLOutletState()
+        return self
+
+    send = __call__  # Alias method name
 
 
 class LSLOutletUnit(ez.Unit):
@@ -52,69 +158,18 @@ class LSLOutletUnit(ez.Unit):
     STATE = LSLOutletState
 
     async def initialize(self) -> None:
-        self._stream_created = False
-        self._clock_sync = ClockSync(run_thread=False)
+        self.create_processor()
 
-    def shutdown(self) -> None:
-        del self.STATE.outlet
-        self.STATE.outlet = None
+    def create_processor(self):
+        self.processor = OutletProcessor(settings=self.SETTINGS)
 
     @ez.task
     async def update_clock(self) -> None:
         while True:
             if self.STATE.outlet is not None:
-                self._clock_sync.run_once()
+                self.processor.state.clock_sync.run_once()
             await asyncio.sleep(0.1)
 
     @ez.subscriber(INPUT_SIGNAL, zero_copy=True)
     async def lsl_outlet(self, msg: AxisArray) -> None:
-        if self.STATE.outlet is None:
-            if isinstance(msg.axes["time"], AxisArray.LinearAxis):
-                fs = 1 / msg.axes["time"].gain
-            else:
-                # Coordinate axis because timestamps are irregular
-                fs = pylsl.IRREGULAR_RATE
-            out_shape = [_[0] for _ in zip(msg.shape, msg.dims) if _[1] != "time"]
-            out_size = int(np.prod(out_shape))
-            info = pylsl.StreamInfo(
-                name=self.SETTINGS.stream_name,
-                type=self.SETTINGS.stream_type,
-                channel_count=out_size,
-                nominal_srate=fs,
-                channel_format=string2fmt[str(msg.data.dtype)],
-                source_id="",  # TODO: Generate a hash from name, type, channel_count, fs, fmt, other metadata...
-            )
-            # Add channel labels to the info desc.
-            if "ch" in msg.axes and isinstance(
-                msg.axes["ch"], AxisArray.CoordinateAxis
-            ):
-                ch_labels = msg.axes["ch"].data
-                # TODO: or get ch_labels from self.SETTINGS.map_file
-                # TODO: if msg is multi-dim then construct labels by combining dims.
-                #  For now, labels only work if only output dims are "time", "ch"
-                if len(ch_labels) == out_size:
-                    chans = info.desc().append_child("channels")
-                    for ch in ch_labels:
-                        chan = chans.append_child("channel")
-                        chan.append_child_value("label", ch)
-                        # TODO: if self.SETTINGS.map_file: Add channel locations
-            self.STATE.outlet = pylsl.StreamOutlet(info)
-
-        if self.STATE.outlet is not None:
-            dat = msg.data
-            if msg.dims[0] != "time":
-                dat = np.moveaxis(dat, msg.dims.index("time"), 0)
-
-            if not dat.flags.c_contiguous:
-                dat = np.ascontiguousarray(dat)
-            if not dat.flags.writeable:
-                # If there is a shared-memory-hop before the LSL outlet then it has made
-                #  the numpy array non-writeable. We need to copy it to a new buffer.
-                dat = np.ascontiguousarray(dat).copy()
-
-            if hasattr(msg.axes["time"], "data"):
-                ts = msg.axes["time"].data
-            else:
-                ts = msg.axes["time"].value(dat.shape[0])
-            ts = self._clock_sync.system2lsl(ts)
-            self.STATE.outlet.push_chunk(dat.reshape(dat.shape[0], -1), timestamp=ts)
+        self.processor(msg)
